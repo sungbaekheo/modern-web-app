40b3e3a7e3287fb9302b24e4bf00cc326b1c6978
sungbaekheo <sungbaek.heo@gmail.com>
2023-07-05 23:38:18 +0900

    Chapter5. Applying JWT

--- cardatabase/src/main/java/com/sungbaek/cardatabase/SecurityConfig.java
+++ cardatabase/src/main/java/com/sungbaek/cardatabase/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.sungbaek.cardatabase;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.sungbaek.cardatabase.service.UserDetailServiceImpl;
 
@@ -19,5 +30,52 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception{
 		auth.userDetailsService(userDetailsService)
 		.passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Autowired
+	private AuthenticationFilter authenticationFilter;
+	
+	@Autowired
+	private AuthEntryPoint exceptionHandler;
+	
+	@Bean
+	public AuthenticationManager getAuthenticationManager() throws Exception{
+		return authenticationManager();
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable()
+		.sessionManagement()
+		.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+		.authorizeRequests()
+		// /login 엔드포인트에 대한 POST 요청은 보호되지 않음
+		.antMatchers(HttpMethod.POST, "/login").permitAll()
+		// 다른 모든 요청은 보호됨
+		.anyRequest().authenticated().and()
+		.exceptionHandling()
+		.authenticationEntryPoint(exceptionHandler).and()
+		.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	// 클래스에 전역 CORS 필터 추가
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		/*
+		 * 출처를 명시적으로 정의하려면 다음과 같이 설정
+		 * 만약 localhost:3000 허용하고자 한다면
+		 * config.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+		 * 와 같은 식으로 작성
+		 */
+		config.setAllowedMethods(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowCredentials(false);
+		config.applyPermitDefaultValues();
+		
+		source.registerCorsConfiguration("/**", config);
+		return source;
 	}
 }
